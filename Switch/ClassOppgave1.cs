1d8d5b0136d1091b5e5a9b739c84ca5582fba89d
mattiasfe <matferge@gmail.com>
2025-05-09 14:50:47 +0200

    Add project files.

---
+++ Switch/ClassOppgave1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class ClassOppgave1
+    {
+        int dag1 = 1;
+
+        public void Run()
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Hvilken dag er det?");
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Mandag");
+                        break;
+                    case "2": 
+                        Console.WriteLine("Det er trisdag");
+                        break;
+                    case "3":
+                        Console.WriteLine("Det er onsdag");
+                        break;
+                    case "4":
+                        Console.WriteLine("Det er torsdag");
+                        break;
+                    case "5":
+                        Console.WriteLine("Det er fredag");
+                        break;
+                    case "6":
+                        Console.WriteLine("Det er laurdag");
+                        break;
+                    case "7":
+                        Console.WriteLine("Det er søndag");
+                        break;
+                } 
+            }
+
+        }
+    }
+}
---
+++ Switch/Program.cs
@@ -0,0 +1,4 @@
+﻿using Switch;
+
+var ClassOppgave1 = new ClassOppgave1();
+ClassOppgave1.Run();
